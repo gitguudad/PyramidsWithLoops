d08a97dd8cf1e242e9d24e03eb18ad68bff47317
farrarad <farrarad@LCBLAB2110-18.uccob.uc.edu>
2019-09-19 08:54:31 -0400

    We are now committing

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -1,24 +1,26 @@
 /*
- * Bill Nicholson
-
- * nicholdw@ucmail.uc.edu
- * Printing pyramids using loops. It kind-of works. 
- * What I really want is
-        1
-       222
-      33333
-     4444444
-    555555555 
+ *Assignment 04
+ *Adam Farrar
+ *farrarad@mail.uc.edu
+ *IS 4010 App Dev w/ Java
+ *Fall 2019
+ *Description: Output pyramid of numbers
+ *Citations: code4coding.com
+ *
 */
 package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int lines = 9;
+		for (int i = 1; i <= lines; i++) {
+			for (int j = 1; j < lines; j++) {
+				System.out.print(" ");
+			}
+			lines--;
+			for(int k=1;k<=i;k++) {
+				System.out.print(i+" ");
 			}
 			System.out.println("");
 		}
